4be2d394a1869b893c5da8f56ca798bdfe59d9f8
MainSilent <70273681+MainSilent@users.noreply.github.com>
2021-04-08 23:50:05 +0430

    proxy functions

--- database.py
+++ database.py
@@ -1,7 +1,9 @@
 import sqlite3
+from proxy_checker import ProxyChecker
 
 conn = sqlite3.connect('Data.db')
 c = conn.cursor()
+checker = ProxyChecker()
 
 class DataBase:
     def __init__(self,username,uID,send):
@@ -66,11 +68,47 @@ class DataBase:
             c.execute("DELETE FROM users WHERE NOT send = 0")
 
 class Proxy:
+    @classmethod
     def add(self, address):
-        ...
+        with conn:
+            c.execute(f"INSERT INTO 'main'.'Proxies'('id','address','used') VALUES (NULL,?,0)",(address,))
+        print("Added\n")
+    
+    @classmethod
+    def get(self, sec = False):
+        if not sec:
+            print("Checking proxies...")
+        with conn:
+            c.execute("SELECT * FROM Proxies")
+            proxies = c.fetchall()
 
-    def check(self, address):
-        ...
+        # return proxy
+        for proxy in proxies:
+            if not proxy[2]:
+                if not checker.check_proxy(proxy[1]):
+                    print("Proxy: "+proxy[1]+" Failed, removing...")
+                    with conn:
+                        c.execute(f"DELETE FROM Proxies WHERE id = {proxy[0]}")
+                        c.execute("SELECT * FROM Proxies")
+                        proxies = c.fetchall()
+                else:
+                    with conn:
+                        c.execute(f"UPDATE Proxies SET used = 1 WHERE id = {proxy[0]}")
+                    print("Proxy: "+proxy[1])
+                    return proxy[1]
 
-    def get(self):
-        ...
+        # check available proxies
+        count = len([proxy for proxy in proxies if not proxy[2]])
+        if not count and not sec:
+            with conn:
+                c.execute(f"UPDATE Proxies SET used = 0")
+            self.get(True)
+        elif not count and sec: 
+            print("No proxy")
+            return False
+
+    @classmethod
+    def count(self):
+        with conn:
+            c.execute("SELECT * FROM Proxies")
+            return len(c.fetchall())
--- main.py
+++ main.py
@@ -2,20 +2,29 @@ import os
 import time 
 from multiprocessing import Process
 from hcaptcha import get_cookie
-from database import DataBase
+from database import DataBase, Proxy
 from client import worker
 
+print(f"{Proxy.count()} Proxy")
 print(f"{DataBase.sentCount()} Users sent\n")
 print("1- Send message to users")
-print("2- Refresh h_captcha cookie")
-print("3- Truncate users")
+print("2- Add proxy")
+print("3- Refresh h_captcha cookie")
+print("4- Truncate users")
 
 choice = int(input("Choose by number: "))
 
 if choice == 1:
 	worker()
 elif choice == 2:
-    get_cookie()
+	print()
+	while True:
+		addr = input("Address: ")
+		if addr.lower() == "q":
+			break
+		Proxy.add(addr)
 elif choice == 3:
+    get_cookie()
+elif choice == 4:
     print("Truncating...")
     DataBase.truncate()
