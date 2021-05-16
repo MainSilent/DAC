b36229f1cc62b5dfb32e4033c07942d4cf77e4de
MainSilent <70273681+MainSilent@users.noreply.github.com>
2021-05-16 16:54:56 +0430

    .

--- main.py
+++ main.py
@@ -4,14 +4,16 @@ from multiprocessing import Process, Manager
 from hcaptcha import get_cookie
 from database import DataBase, Proxy
 from client import worker
+from channel import fetch
 from dotenv import load_dotenv; load_dotenv()
 
 print(f"{Proxy.count()} Proxy")
 print(f"{DataBase.sentCount()} Users sent\n")
 print("1- Send message to users")
-print("2- Add proxy")
-print("3- Refresh h_captcha cookie")
-print("4- Truncate sent")
+print("2- Fetch users")
+print("3- Add proxy")
+print("4- Refresh h_captcha cookie")
+print("5- Truncate sent")
 
 manager = Manager()
 scroll = manager.Value('scroll', 0)
@@ -38,14 +40,16 @@ if choice == 1:
 			print("Waiting for next worker...\n")
 			time.sleep(110)
 elif choice == 2:
+	fetch()
+elif choice == 3:
 	print()
 	while True:
 		addr = input("Address: ")
 		if addr.lower() == "q":
 			break
 		Proxy.add(addr)
-elif choice == 3:
-    get_cookie()
 elif choice == 4:
+    get_cookie()
+elif choice == 5:
     print("Truncating...")
     DataBase.truncate()
