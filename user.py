4be2227f843aa3cd0684607084a8310b01249371
7BxDWoKVT4 <82365746+7BxDWoKVT4@users.noreply.github.com>
2021-06-22 21:32:18 +0430

    Update user.py

--- user.py
+++ user.py
@@ -1,5 +1,6 @@
 import json
 import requests
+from random_username.generate import generate_username
 
 headers = {
   'X-Track': ' eyJvcyI6IkxpbnV4IiwiYnJvd3NlciI6IkNocm9tZSIsImRldmljZSI6IiIsInN5c3RlbV9sb2NhbGUiOiJlbi1VUyIsImJyb3dzZXJfdXNlcl9hZ2VudCI6Ik1vemlsbGEvNS4wIChYMTE7IExpbnV4IHg4Nl82NCkgQXBwbGVXZWJLaXQvNTM3LjM2IChLSFRNTCwgbGlrZSBHZWNrbykgQ2hyb21lLzkxLjAuNDQ3Mi43NyBTYWZhcmkvNTM3LjM2IiwiYnJvd3Nlcl92ZXJzaW9uIjoiOTEuMC40NDcyLjc3Iiwib3NfdmVyc2lvbiI6IiIsInJlZmVycmVyIjoiIiwicmVmZXJyaW5nX2RvbWFpbiI6IiIsInJlZmVycmVyX2N1cnJlbnQiOiIiLCJyZWZlcnJpbmdfZG9tYWluX2N1cnJlbnQiOiIiLCJyZWxlYXNlX2NoYW5uZWwiOiJzdGFibGUiLCJjbGllbnRfYnVpbGRfbnVtYmVyIjo5OTk5LCJjbGllbnRfZXZlbnRfc291cmNlIjpudWxsfQ==',
@@ -10,6 +11,10 @@ headers = {
 register_url = "https://discord.com/api/v9/auth/register"
 
 def create():
+	payload = json.dumps({
+		"consent": True,
+		"username": generate_username(1)[0],
+		"captcha_key": ""
+	})
 	response = requests.request("POST", url, headers=headers, data=payload)
-
 	print(response.text)
