0a6e5531a39cb005df7f75ba1bbd5642406eb80a
silent <82365746+7BxDWoKVT4@users.noreply.github.com>
2021-06-23 21:16:14 +0430

    Update main.py

--- main.py
+++ main.py
@@ -11,9 +11,16 @@ choice = int(input("Choose by number: "))
 if choice == 1:
 	r = int(input("Enter number of users ( -1 for unlimited ): "))
 	tor = input("Enable tor proxy?[y/n]: ").lower()
-	
+
 	while (True if r == -1 else False) or (DataBase.Count() <= r):
 		print()
+
+		if tor == 'y':
+			...
+
 		create()
+
+		if tor == 'y':
+			...
 elif choice == 2:
     get_cookie()
