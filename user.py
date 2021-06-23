fa6e4db66a64a7edb70d4e2645658abe21488e59
silent <82365746+7BxDWoKVT4@users.noreply.github.com>
2021-06-23 19:00:42 +0430

    Update user.py

--- user.py
+++ user.py
@@ -45,29 +45,30 @@ def join(token):
         return False
 
 def create():
-	captcha_key = hcaptcha.new()
-	if not captcha_key:
-		raise Exception("Failed to get captcha key")
-
-	# For now we only want to invite, for not getting accounts ban uncomment the headers
-	headers = {
-		'Content-Type': 'application/json',
-		#'Cookie': '__dcfduid=61f8ced90af24ebc9736ccf74c566850',
-		#'X-Track': 'eyJvcyI6IkxpbnV4IiwiYnJvd3NlciI6IkNocm9tZSIsImRldmljZSI6IiIsInN5c3RlbV9sb2NhbGUiOiJlbi1VUyIsImJyb3dzZXJfdXNlcl9hZ2VudCI6Ik1vemlsbGEvNS4wIChYMTE7IExpbnV4IHg4Nl82NCkgQXBwbGVXZWJLaXQvNTM3LjM2IChLSFRNTCwgbGlrZSBHZWNrbykgQ2hyb21lLzkxLjAuNDQ3Mi43NyBTYWZhcmkvNTM3LjM2IiwiYnJvd3Nlcl92ZXJzaW9uIjoiOTEuMC40NDcyLjc3Iiwib3NfdmVyc2lvbiI6IiIsInJlZmVycmVyIjoiIiwicmVmZXJyaW5nX2RvbWFpbiI6IiIsInJlZmVycmVyX2N1cnJlbnQiOiIiLCJyZWZlcnJpbmdfZG9tYWluX2N1cnJlbnQiOiIiLCJyZWxlYXNlX2NoYW5uZWwiOiJzdGFibGUiLCJjbGllbnRfYnVpbGRfbnVtYmVyIjo5OTk5LCJjbGllbnRfZXZlbnRfc291cmNlIjpudWxsfQ=='
-	}
-	user = generate_username(1)[0]
-	payload = json.dumps({
-		"username": user,
-		"email": f"{user}@gmail.com",
-		"password": password_gen(),
-		"date_of_birth": "1998-05-14",
-		"consent": True,
-		"invite": invite if invite else None,
-		"gift_code_sku_id": None,
-		"captcha_key": captcha_key
-	})
-	response = requests.request("POST", register_url, headers=headers, data=payload)
 	try:
+		response = None
+		user = generate_username(1)[0]
+		captcha_key = hcaptcha.new()
+		if not captcha_key:
+			raise Exception("Failed to get captcha key")
+
+		# For now we only want to invite, for not getting accounts ban uncomment the headers
+		headers = {
+			'Content-Type': 'application/json',
+			#'Cookie': '__dcfduid=61f8ced90af24ebc9736ccf74c566850',
+			#'X-Track': 'eyJvcyI6IkxpbnV4IiwiYnJvd3NlciI6IkNocm9tZSIsImRldmljZSI6IiIsInN5c3RlbV9sb2NhbGUiOiJlbi1VUyIsImJyb3dzZXJfdXNlcl9hZ2VudCI6Ik1vemlsbGEvNS4wIChYMTE7IExpbnV4IHg4Nl82NCkgQXBwbGVXZWJLaXQvNTM3LjM2IChLSFRNTCwgbGlrZSBHZWNrbykgQ2hyb21lLzkxLjAuNDQ3Mi43NyBTYWZhcmkvNTM3LjM2IiwiYnJvd3Nlcl92ZXJzaW9uIjoiOTEuMC40NDcyLjc3Iiwib3NfdmVyc2lvbiI6IiIsInJlZmVycmVyIjoiIiwicmVmZXJyaW5nX2RvbWFpbiI6IiIsInJlZmVycmVyX2N1cnJlbnQiOiIiLCJyZWZlcnJpbmdfZG9tYWluX2N1cnJlbnQiOiIiLCJyZWxlYXNlX2NoYW5uZWwiOiJzdGFibGUiLCJjbGllbnRfYnVpbGRfbnVtYmVyIjo5OTk5LCJjbGllbnRfZXZlbnRfc291cmNlIjpudWxsfQ=='
+		}
+		payload = json.dumps({
+			"username": user,
+			"email": f"{user}@gmail.com",
+			"password": password_gen(),
+			"date_of_birth": "1998-05-14",
+			"consent": True,
+			"invite": invite if invite else None,
+			"gift_code_sku_id": None,
+			"captcha_key": captcha_key
+		})
+		response = requests.request("POST", register_url, headers=headers, data=payload)
 		#is_valid(json.loads(response.text)["token"])
 		if json.loads(response.text)["token"]:
 			newUser = DataBase(user, 0, 0)
