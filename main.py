93a80bb4b19cf8c0ef92756eb7641e938a141445
silent <82365746+7BxDWoKVT4@users.noreply.github.com>
2021-06-25 12:49:02 +0430

    Update main.py

--- main.py
+++ main.py
@@ -1,20 +1,14 @@
 import os
 from user import create
 from database import DataBase
-from captcha_token import get_cookie
 
 print(f"{DataBase.Count()} Users\n")
 print("1- Create users")
 print("2- Refresh h_captcha cookie")
 
-choice = int(input("Choose by number: "))
+r = int(input("Enter number of users ( -1 for unlimited ): "))
+tor = input("Enable tor proxy?[y/n]: ").lower()
 
-if choice == 1:
-	r = int(input("Enter number of users ( -1 for unlimited ): "))
-	tor = input("Enable tor proxy?[y/n]: ").lower()
-
-	while (True if r == -1 else False) or (DataBase.Count() <= r):
-		print()
-		create(True if tor == 'y' else False)
-elif choice == 2:
-    get_cookie()
+while (True if r == -1 else False) or (DataBase.Count() <= r):
+	print()
+	create(True if tor == 'y' else False)
