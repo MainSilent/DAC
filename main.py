4cc5e3877253ea75a324fcb234baae6f86c399da
silent <82365746+7BxDWoKVT4@users.noreply.github.com>
2021-06-25 09:51:56 +0430

    Update main.py

--- main.py
+++ main.py
@@ -14,15 +14,8 @@ except:
 	choice = int(input("Choose by number: "))
 
 if choice == 1:
-	#r = int(input("Enter number of users ( -1 for unlimited ): "))
-	#tor = input("Enable tor proxy?[y/n]: ").lower()
-	tor = 'y'; r = -1
-
-	if tor == 'y':
-		if os.system("kalitorify -t") != 0:
-			print("Failed to start tor")
-		else:
-			print("Tor Started")
+	r = int(input("Enter number of users ( -1 for unlimited ): "))
+	tor = input("Enable tor proxy?[y/n]: ").lower()
 
 	while (True if r == -1 else False) or (DataBase.Count() <= r):
 		print()
