459457a1f1dd3c552f6d4945ab6d218b59138ad7
silent <82365746+7BxDWoKVT4@users.noreply.github.com>
2021-06-25 08:40:03 +0430

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,23 +1,21 @@
-# Image
+# Prerequisite
 FROM ubuntu:20.04
-RUN apt-get update
-RUN apt-get install -y git tor curl python3 make python3-pip wget
-RUN DEBIAN_FRONTEND="noninteractive" apt-get -y install tzdata
+apt-get update
+apt-get install -y git tor curl python3 make python3-pip wget
+DEBIAN_FRONTEND="noninteractive" apt-get -y install tzdata
 
 # Kalitorify
-RUN git clone https://github.com/brainfucksec/kalitorify.git
-RUN cd kalitorify && make install && cd .. && rm -rf kalitorify
+git clone https://github.com/brainfucksec/kalitorify.git
+cd kalitorify && make install && cd .. && rm -rf kalitorify
 
 # Chrome
-ARG CHROME_VERSION="89.0.4389.90-1"
-RUN wget -O /tmp/chrome.deb https://dl.google.com/linux/chrome/deb/pool/main/g/google-chrome-stable/google-chrome-stable_${CHROME_VERSION}_amd64.deb \
+CHROME_VERSION="89.0.4389.90-1"
+wget -O /tmp/chrome.deb https://dl.google.com/linux/chrome/deb/pool/main/g/google-chrome-stable/google-chrome-stable_${CHROME_VERSION}_amd64.deb \
   && apt-get install -y /tmp/chrome.deb \
   && rm /tmp/chrome.deb
 
 # Requirements
-COPY requirements.txt .
-RUN pip3 install -r requirements.txt
+pip3 install -r requirements.txt
 
-# Run 
-COPY . .
-RUN python3 main.py 1
+# Run
+python3 main.py 1
