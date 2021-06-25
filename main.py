772b9997aa28b3bbba749a02702ddd694b3331cc
silent <82365746+7BxDWoKVT4@users.noreply.github.com>
2021-06-25 17:07:53 +0430

    Update main.py

--- main.py
+++ main.py
@@ -8,7 +8,7 @@ print(f"{DataBase.Count()} Users\n")
 r = int(input("Enter number of users ( -1 for unlimited ): "))
 tor = input("Enable tor proxy?[y/n]: ").lower()
 
-while (True if r == -1 else False) or (DataBase.Count() <= r):
+while (True if r == -1 else False) or (DataBase.Count() < r):
 	print()
 	p = Process(target=create)
 	p.start()
