93bf368ccf8c711c181c68e797c40d36e98f7de5
Silent <hamidreza.neo@gmail.com>
2021-08-05 11:20:25 +0430

    Update run.sh

--- run.sh
+++ run.sh
@@ -11,8 +11,8 @@ CHROME_VERSION="89.0.4389.90-1"
 wget -O /tmp/chrome.deb https://dl.google.com/linux/chrome/deb/pool/main/g/google-chrome-stable/google-chrome-stable_${CHROME_VERSION}_amd64.deb \
   && apt-get install -y /tmp/chrome.deb \
   && rm /tmp/chrome.deb
-wget "https://chromedriver.storage.googleapis.com/89.0.4389.23/chromedriver_linux64.zip"
-  && unzip chromedriver_linux64.zip
+wget "https://chromedriver.storage.googleapis.com/89.0.4389.23/chromedriver_linux64.zip" \
+  && unzip chromedriver_linux64.zip \
   && mv chromedriver /bin
 
 # Requirements
